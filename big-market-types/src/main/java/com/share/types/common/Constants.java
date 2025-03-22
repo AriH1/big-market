7f20103099b2ab8c1f9bed93d62064f10287d78e
MUZ <2826087503@qq.com>
2025-03-22 11:10:41 +0800

    40006000积分装配策略

--- big-market-types/src/main/java/com/share/types/common/Constants.java
+++ big-market-types/src/main/java/com/share/types/common/Constants.java
@@ -3,8 +3,11 @@ package com.share.types.common;
 public class Constants {
 
     public final static String SPLIT = ",";
+    public static final String SPACE = " " ;
+    public static final String COLON = ":";
 
     public static class RedisKey{
+        public static String STRATEGY_KEY = "big-market-strategy-key-" ;
         public static String STRATEGY_AWARD_KEY = "big-market-strategy-award-key-";
         public static String STRATEGY_RATE_TABLE_KEY = "big-market-strategy-rate-table-key-";
         public static String STRATEGY_RATE_RANGE_KEY = "big-market-strategy-rate-range-key-";
